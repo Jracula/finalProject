115e927d88a962075118a34d4616a98a924d0c38
hongmee <janet195@naver.com>
2023-05-07 17:15:05 +0900

    새로운 DB연결

--- finalproject_teammoong/src/main/java/common/Error.java
+++ finalproject_teammoong/src/main/java/common/Error.java
@@ -8,23 +8,23 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 
-@ControllerAdvice
-public class Error {
-
-	@ExceptionHandler(Exception.class)
-	public String handleException(Exception e) {
-		return "common/error500";
-	}
-	
-	
-	
-	
-	@ExceptionHandler(NoHandlerFoundException.class) //이런 Exception이 발생하고
-	@ResponseStatus(value = HttpStatus.NOT_FOUND) //그게 not found일때
-	public String noFound(NoHandlerFoundException e) {
-		return "common/error400";
-	}
-	
-	
-	
-}
+//@ControllerAdvice
+//public class Error {
+//
+//	@ExceptionHandler(Exception.class)
+//	public String handleException(Exception e) {
+//		return "common/error500";
+//	}
+//	
+//	
+//	
+//	
+//	@ExceptionHandler(NoHandlerFoundException.class) //이런 Exception이 발생하고
+//	@ResponseStatus(value = HttpStatus.NOT_FOUND) //그게 not found일때
+//	public String noFound(NoHandlerFoundException e) {
+//		return "common/error400";
+//	}
+//	
+//	
+//	
+//}
